05d052aa18c897ac6420127897af6f62140bb790
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-09 11:49:57 -0800

    FEAT: New problem 1.7 from chapter 1.

---
+++ chapter_1/p1.7_rotate_matrix.js
@@ -0,0 +1,43 @@
+/*
+Rotate Matrix
+Given an image represented by an N X N matrix, where each pixel on the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place?
+*/
+
+/*
+Explaination:
+
+*/
+
+/*
+Questions:
+
+*/
+
+/*
+Assumption:
+The outer array always contains atleast one array inside.
+*/
+
+/*
+Example:
+1) [[1]] -> [[1]]
+2) [[]] -> [[]]
+3) [[ , ], [ , ]] -> [[ , ], [ , ]]
+4) [[1,2,3], [4,5,6], [7,8,9]] -> [[1,4,], [2,5,8], [3,6,9]]
+123
+456
+789
+741
+852
+963
+5) [] -> []
+6) [[1], [2,3]] -> [[1], [2,3]]
+*/
+
+
+//Sol1
+const rotateMatrixSol1 = (sqMatArray) => {
+
+
+
+}
