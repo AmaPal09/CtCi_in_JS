5147b1f00dbdfa0ad463b7f6f61cdd5257eab2b1
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-09 11:20:26 -0800

    CHNG: Add another solution just using strings

--- chapter_1/p1.6_String_Compression.js
+++ chapter_1/p1.6_String_Compression.js
@@ -59,9 +59,49 @@ const strCompressSol1 = (inStr) => {
 		return inStr;
 	}
 }
-
+//Space O(n) additional array of atleast string length
+//Time: O(n)
 console.log(strCompressSol1("aabcccccaaa")); //a2b1c5a3
 console.log(strCompressSol1("abcd")); // abcd
 console.log(strCompressSol1("aabbccc")); // a2b2c1
 console.log(strCompressSol1("")); // ""
 console.log(strCompressSol1("aaAAAbCCcccaaAAA")); // a2A3b1C2c3a2A3
+
+
+// Sol2
+const strCompressSol2 = (inStr) => {
+	if (inStr.length < 1) {
+		return inStr;
+	}
+	else if (inStr === " ") {
+		return inStr;
+	}
+
+
+	let compressedStr = "";
+	let currChar = "";
+	let currCount = "";
+
+	for (let i = 0; i < inStr.length; i++) {
+		if (currChar !== inStr[i]) {
+			compressedStr = compressedStr + currChar + currCount;
+			console.log(compressedStr);
+			currChar = inStr[i];
+			currCount = 1;
+		}
+		else {
+			currCount++;
+		}
+
+		if (i === inStr.length - 1) {
+			compressedStr = compressedStr + currChar + currCount;
+		}
+	}
+
+	return compressedStr.length < inStr.length ? compressedStr : inStr;
+}
+console.log(strCompressSol2("aabcccccaaa")); //a2b1c5a3
+console.log(strCompressSol2("abcd")); // abcd
+console.log(strCompressSol2("aabbccc")); // a2b2c1
+console.log(strCompressSol2("")); // ""
+console.log(strCompressSol2("aaAAAbCCcccaaAAA")); // a2A3b1C2c3a2A3
