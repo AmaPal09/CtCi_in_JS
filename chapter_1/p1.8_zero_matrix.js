09715836b5daeaa1e1adc217984f6aa31498b6cf
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-09 12:26:20 -0800

    FEAT: Add problem 8 statement from chapter 1.

---
+++ chapter_1/p1.8_zero_matrix.js
@@ -0,0 +1,22 @@
+/*
+Zero Matrix
+Write an algorithm sich that if an element in an MxN jmatrix is 0, its entire row and column are set to 0;
+*/
+
+/*
+Explaination:
+
+*/
+
+/*
+Questions:
+*/
+
+/*
+Assumption
+*/
+
+/*
+Example:
+1)
+*/
