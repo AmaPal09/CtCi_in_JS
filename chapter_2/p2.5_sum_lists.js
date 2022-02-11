689022d6025704682c82c55fbe742efe27fb11aa
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-11 08:15:40 -0800

    FEAT: Add  problem, p2.5 from chapter 2

---
+++ chapter_2/p2.5_sum_lists.js
@@ -0,0 +1,28 @@
+/*
+Sum Lists
+You have two numbers represented by linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list. (You are not allowed to cheat and just convert the linked list to an integer)
+*/
+
+/*
+Explaination:
+*/
+
+/*
+Questions:
+1) Can the LL contain -ve numbers?
+2) Can any of the 2 LLs be null?
+*/
+
+/*
+Assumption:
+1) LL contains only non -ve numbers
+2) LL contains atleast 0 or null.
+3)
+*/
+
+/*
+Example:
+1)  + 1->2 => 1->2
+2) 0 + 1->2 => 1->2
+3) 1->2-3 + 4->5->6 => 5->7->9
+*/
