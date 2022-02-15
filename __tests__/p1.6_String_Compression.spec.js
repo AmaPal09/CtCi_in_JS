4a7c2c93b0d084533622657aa62c77f000c37a8b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-15 11:09:43 -0800

    CHNG: Add test cases for sol1 and sol2

--- __tests__/p1.6_String_Compression.spec.js
+++ __tests__/p1.6_String_Compression.spec.js
@@ -1,58 +1,67 @@
 import {strCompressSol1} from "./../chapter_1/p1.6_String_Compression.js";
 import {strCompressSol2} from "./../chapter_1/p1.6_String_Compression.js";
 
-describe("Validate sol1 identifies whether the strings are 1 edit away", () => {
-	test("Sol1 returns true when strings are 1 replacement away", () => {
-		let str1 = "pale";
-		let str2 = "bale";
-		expect(oneAwaySol1(str1, str2)).toBeTruthy();
-	});
-
-	test("Sol1 returns true when strings are 1 insertion away", () => {
-		let str1 = "pale";
-		let str2 = "paale";
-		expect(oneAwaySol1(str1, str2)).toBeTruthy();
-	});
-
-	test("Sol1 returns true when strings are 1 insertion away", () => {
+describe("Validate sol1 compresses ", () => {
+	test("Sol1 returns empty string when input string is empty", () => {
 		let str1 = "";
-		let str2 = "p";
-		expect(oneAwaySol1(str1, str2)).toBeTruthy();
+		expect(strCompressSol1(str1)).toMatch("");
 	});
 
-	test("Sol1 returns true when strings are 1 deletion away", () => {
-		let str1 = "pale";
-		let str2 = "ple";
-		expect(oneAwaySol1(str1, str2)).toBeTruthy();
-	});
-
-	test("Sol1 returns false when strings are more than 1 change away", () => {
-		let str1 = "pale";
-		let str2 = "bales";
-		expect(oneAwaySol1(str1, str2)).toBeFalsy();
-	});
-
-	test("Sol1 returns false when atleast one string is null", () => {
+	test("Sol1 returns empty string when input string is null", () => {
 		let str1 = null;
-		let str2 = "pale";
-		expect(oneAwaySol1(str1, str2)).toBeFalsy();
+		expect(strCompressSol1(str1)).toMatch("");
 	});
 
-	test("Sol1 returns false when atleast one string is undefined", () => {
-		let str1 = "pale";
-		let str2 = undefined;
-		expect(oneAwaySol1(str1, str2)).toBeFalsy();
-	});
-
-	test("Sol1 returns false when both strings are undefined", () => {
+	test("Sol1 returns empty string when input string is undefined", () => {
 		let str1 = undefined;
-		let str2 = undefined;
-		expect(oneAwaySol1(str1, str2)).toBeFalsy();
+		expect(strCompressSol1(str1)).toMatch("");
 	});
 
-	test("Sol1 returns false when both strings are null", () => {
-		let str1 = null;
+	test("Sol1 returns compressed string when compressed string is smaller than input string", () => {
+		let str1 = "aabcccccaaa";
+		expect(strCompressSol1(str1)).toMatch("a2b1c5a3");
+	});
+
+	test("Sol1 returns input string when compressed string is bigger than input strings", () => {
+		let str1 = "abcd";
+		expect(strCompressSol1(str1)).toMatch("abcd");
+	});
+
+	test("Sol1 returns input string when compressed string is bigger than input strings", () => {
+		let str1 = "aaAAAbCCcccaaAAA";
+		expect(strCompressSol1(str1)).toMatch("a2A3b1C2c3a2A3");
+	});
+});
+
+
+describe("Validate sol2 compresses ", () => {
+	test("Sol2 returns empty string when input string is empty", () => {
+		let str2 = "";
+		expect(strCompressSol2(str2)).toMatch("");
+	});
+
+	test("Sol2 returns empty string when input string is null", () => {
 		let str2 = null;
-		expect(oneAwaySol1(str1, str2)).toBeFalsy();
+		expect(strCompressSol2(str2)).toMatch("");
+	});
+
+	test("Sol2 returns empty string when input string is undefined", () => {
+		let str2 = undefined;
+		expect(strCompressSol2(str2)).toMatch("");
+	});
+
+	test("Sol2 returns compressed string when compressed string is smaller than input string", () => {
+		let str2 = "aabcccccaaa";
+		expect(strCompressSol2(str2)).toMatch("a2b1c5a3");
+	});
+
+	test("Sol2 returns input string when compressed string is bigger than input strings", () => {
+		let str2 = "abcd";
+		expect(strCompressSol2(str2)).toMatch("abcd");
+	});
+
+	test("Sol2 returns input string when compressed string is bigger than input strings", () => {
+		let str2 = "aaAAAbCCcccaaAAA";
+		expect(strCompressSol2(str2)).toMatch("a2A3b1C2c3a2A3");
 	});
 });
