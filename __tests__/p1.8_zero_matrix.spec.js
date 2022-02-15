e0d317b9b20312b5bffac000c24b1cf3b87828db
Amarja Palekar <amarjapalekar09@gmail.com>
2022-02-15 15:26:05 -0800

    CHNG: Add test cases for sol2

--- __tests__/p1.8_zero_matrix.spec.js
+++ __tests__/p1.8_zero_matrix.spec.js
@@ -42,38 +42,38 @@ describe("Validate sol1 returns a matrix with all 0s in the rows and columns con
 });
 
 describe("Validate sol2 returns a matrix with all 0s in the rows and columns containing a zero", () => {
-	test("Sol1 returns input when input matrix is blank", () => {
+	test("Sol2 returns input when input matrix is blank", () => {
 		let mat = [];
-		expect(zeroMatSol1(mat)).toEqual([]);
+		expect(zeroMatSol2(mat)).toEqual([]);
 	});
 
-	test("Sol1 returns input when input matrix contains empty row", () => {
+	test("Sol2 returns input when input matrix contains empty row", () => {
 		let mat = [[]];
-		expect(zeroMatSol1(mat)).toEqual([[]]);
+		expect(zeroMatSol2(mat)).toEqual([[]]);
 	});
 
-	test("Sol1 returns empty matrix when input matrix is null", () => {
+	test("Sol2 returns empty matrix when input matrix is null", () => {
 		let mat = null;
-		expect(zeroMatSol1(mat)).toEqual([]);
+		expect(zeroMatSol2(mat)).toEqual([]);
 	});
 
-	test("Sol1 returns empty matrix when input matrix is undefined", () => {
+	test("Sol2 returns empty matrix when input matrix is undefined", () => {
 		let mat = undefined;
-		expect(zeroMatSol1(mat)).toEqual([]);
+		expect(zeroMatSol2(mat)).toEqual([]);
 	});
 
-	test("Sol1 returns input matrix when input matrix has no zero", () => {
+	test("Sol2 returns input matrix when input matrix has no zero", () => {
 		let mat = [[1], [2,3]];
-		expect(zeroMatSol1(mat)).toEqual([[1], [2,3]]);
+		expect(zeroMatSol2(mat)).toEqual([[1], [2,3]]);
 	});
 
-	test("Sol1 returns zero matrix when input matrix has a zero", () => {
+	test("Sol2 returns zero matrix when input matrix has a zero", () => {
 		let mat = [
 					[1,2,3,4,5],
 					[6,7,8,9,10],
 					[11,12,0,14,15]
 				];
-		expect(zeroMatSol1(mat)).toEqual([
+		expect(zeroMatSol2(mat)).toEqual([
 												[1,2,0,4,5],
 												[6,7,0,9,10],
 												[0,0,0,0,0]
