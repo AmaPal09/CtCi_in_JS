7e2cae4a7902d7a7e6f4be59b4f5a4b2e1c69314
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-23 22:01:09 -0700

    Change spaces in comments

--- chapter_1/p1.8_zero_matrix.js
+++ chapter_1/p1.8_zero_matrix.js
@@ -1,6 +1,6 @@
 /*
 Zero Matrix
-Write an algorithm sich that if an element in an MxN jmatrix is 0, its entire row and column are set to 0;
+Write an algorithm such that if an element in an MxN jmatrix is 0, its entire row and column are set to 0;
 */
 
 /*
