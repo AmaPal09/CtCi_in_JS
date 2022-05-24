03075b303b41325a247ac0fbc7af6df683fb3208
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-24 14:58:23 -0700

    Use classes from linked list files and test using just notes and linked list both

--- __tests__/p2.1_remove_dups.spec.js
+++ __tests__/p2.1_remove_dups.spec.js
@@ -5,38 +5,73 @@ import {LinkedList} from "./../chapter_2/linkedList.js";
 
 
 describe("Validate sol1 remove dups", () => {
-	test("Validate Sol1 returns intersecting node when found", () => {
-			let n1 = new ListNode(1);
-			let ll1 = new LinkedList(n1);
-			ll1.add(2);
-			ll1.add(33);
-			ll1.add(99);
-			ll1.add(66);
-			ll1.add(55);
-			ll1.add(33);
-			ll1.add(77);
-			let n2 = new ListNode(1);
-			let ll2 = new LinkedList(n2);
-			ll2.add(2);
-			ll2.add(33);
-			ll2.add(99);
-			ll2.add(66);
-			ll2.add(55);
-			ll2.add(77);
 
-			expect(removeDupsSol1(n1)).toEqual(ll2.head);
-			let n3 = new ListNode(1);
-			let ll3 = new LinkedList(n3);
-			ll3.add(99);
-			ll3.add(66);
-			ll3.add(55);
-			ll3.add(77);
-			let n4 = new ListNode(1);
-			let ll4 = new LinkedList(n4);
-			ll4.add(99);
-			ll4.add(66);
-			ll4.add(55);
-			ll4.add(77);
-			expect(removeDupsSol1(ll3.head)).toEqual(ll4.head);
+	test("Validate Sol1 returns intersecting node when found", () => {
+			let a1 = new ListNode(1);
+			let b1 = new ListNode(2);
+			let c1 = new ListNode(3);
+			let d1 = new ListNode(2);
+			let e1 = new ListNode(4);
+			let f1 = new ListNode(3);
+			console.log(a1);
+
+			let g1 = new ListNode(1);
+			let h1 = new ListNode(2);
+			let i1 = new ListNode(3);
+			let j1 = new ListNode(4);
+			console.log(g1);
+
+			removeDupsSol1(a1);
+			expect(a1).toEqual(g1);
 	});
+
+	test("Validate Sol1 does not delete anything when no duplicates are found", () => {
+			let k1 = new ListNode(1);
+			let l1 = new ListNode(2);
+			let m1 = new ListNode(3);
+			let n1 = new ListNode(4);
+			console.log(k1);
+
+			let o1 = new ListNode(1);
+			let p1 = new ListNode(2);
+			let q1 = new ListNode(3);
+			let r1 = new ListNode(4);
+			console.log(o1)
+
+			removeDupsSol1(k1);
+			expect(k1).toEqual(o1);
+	});
+
+
+	test("Validate Sol1 for new class", () => {
+			let a1 = new ListNode(1);
+			let lla = new LinkedList(a1);
+			lla.addMultiple([2,3,2,4,3]);
+			console.log(lla.printList());
+
+			let g1 = new ListNode(1);
+			let llg = new LinkedList(g1);
+			llg.addMultiple(2,3,4);
+			console.log(llg.printList());
+
+			removeDupsSol1(lla.head);
+			expect(lla.head).toEqual(llg.head);
+	});
+
+	// test("Validate Sol1 does not delete anything when no duplicates are found", () => {
+	// 		let k1 = new ListNode(1);
+	// 		let l1 = new ListNode(2);
+	// 		let m1 = new ListNode(3);
+	// 		let n1 = new ListNode(4);
+	// 		console.log(k1);
+
+	// 		let o1 = new ListNode(1);
+	// 		let p1 = new ListNode(2);
+	// 		let q1 = new ListNode(3);
+	// 		let r1 = new ListNode(4);
+	// 		console.log(o1)
+
+	// 		removeDupsSol1(k1);
+	// 		expect(k1).toEqual(o1);
+	// });
 });
