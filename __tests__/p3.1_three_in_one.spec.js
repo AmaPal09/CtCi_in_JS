ce7ef1e75112d572963e610fdfd83c094761c116
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-02 11:36:09 -0700

    Test push for individual stacks

--- __tests__/p3.1_three_in_one.spec.js
+++ __tests__/p3.1_three_in_one.spec.js
@@ -37,3 +37,37 @@ describe("Validate ThreeInOne holds three stacks with 3 sets of commands in one 
 			console.log(tio.container);
 	});
 });
+
+
+
+describe("Validate middle stack in ThreeInOne functions even when other 2 stacks are not filled", () => {
+	let tio = new ThreeInOne();
+	test("Validate values added in the middle stack", () => {
+			tio.pushMid('2a');
+			expect(tio.container).toEqual(['2a'])
+			tio.pushMid('2b');
+			expect(tio.container).toEqual(['2b', '2a']);
+			tio.pushMid('2c');
+			expect(tio.container).toEqual(['2c', '2b', '2a']);
+			console.log(tio.container);
+			expect(tio.middleTop).toEqual(0);
+			expect(tio.middleBottom).toEqual(3);
+	});
+});
+
+
+
+describe("Validate bottom stack in ThreeInOne functions even when other 2 stacks are not filled", () => {
+	let tio = new ThreeInOne();
+	test("Validate values added in the middle stack", () => {
+			tio.pushBottom('3a');
+			expect(tio.container).toEqual([ '3a',])
+			tio.pushBottom('3b');
+			expect(tio.container).toEqual([ '3b', '3a']);
+			tio.pushBottom('3c');
+			expect(tio.container).toEqual(['3c', '3b', '3a']);
+			console.log(tio.container);
+			expect(tio.middleTop).toEqual(0);
+			expect(tio.middleBottom).toEqual(0);
+	});
+});
