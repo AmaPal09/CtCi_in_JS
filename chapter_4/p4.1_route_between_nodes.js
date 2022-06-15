62bc42dc74866470844b5f2248904e0e9a8cdd8b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-15 12:41:20 -0700

    Add problem 1 from chapter 4

---
+++ chapter_4/p4.1_route_between_nodes.js
@@ -0,0 +1,30 @@
+/*
+Route Between Nodes
+Given a directed graph and two nodes (S and E), design an algorithm to find ouit whether there is a route from S to E
+*/
+
+/*
+Explaination:
+Can use BFS or DFS to traverse through the graph.
+DFS is simple recursion but DFS will search at least one level deeper than the target node level as it will access nodes depthwise
+BFS is a queue so extra datastructure but it will find the path/route faster as
+as it will search closer to the starting node first and move away only when and 1 level every time it is necessary
+*/
+
+/*
+Questions:
+1) What happens when no route present?
+2) What if target node is not found?
+3) What if graph is empty?
+4)
+*/
+
+/*
+Assumption:
+1)
+*/
+
+/*
+Example:
+1)
+*/
