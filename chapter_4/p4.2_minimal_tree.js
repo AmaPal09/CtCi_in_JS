34c0b79a695416a60b8ba28a0ca213618e063161
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-08 12:28:39 -0700

    Add problem 2 from chapter 4

---
+++ chapter_4/p4.2_minimal_tree.js
@@ -0,0 +1,23 @@
+/*
+Minimal Tree
+Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+*/
+
+/*
+Explaination:
+*/
+
+/*
+Questions:
+1)
+*/
+
+/*
+Assumption:
+1)
+*/
+
+/*
+Example:
+1)
+*/
