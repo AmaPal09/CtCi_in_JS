8e06d767c8d0b8c932e5582bdf290e46a92b8a86
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-08 16:04:51 -0700

    Add solution for problem 2

--- chapter_4/p4.2_minimal_tree.js
+++ chapter_4/p4.2_minimal_tree.js
@@ -118,13 +118,54 @@ class BST {
 }
 
 // [1,2,3,4,5,6,7]
-let bst1 = new BST();
-bst1.insert(4);
-bst1.insert(2);
-bst1.insert(6);
-bst1.insert(1);
-bst1.insert(3);
-bst1.insert(5);
-bst1.insert(7);
+// let bst1 = new BST();
+// bst1.insert(4);
+// bst1.insert(2);
+// bst1.insert(6);
+// bst1.insert(1);
+// bst1.insert(3);
+// bst1.insert(5);
+// bst1.insert(7);
 
-bst1.printInLevelOrder();
+// bst1.printInLevelOrder();
+
+
+const minimal_tree = (sorted_array) => {
+	// if (sorted_array.length === 0) {
+	// 	return null;
+	// }
+
+	let bst = new BST();
+	let q = new Queue();
+	let currArray;
+	q.enqueue(sorted_array);
+
+	while(!q.isEmpty()) {
+		currArray = q.dequeue();
+		bst.insert(currArray[Math.floor(currArray.length/2)]);
+		if(currArray.slice(0, Math.floor(currArray.length/2)).length > 0) {
+			q.enqueue(currArray.slice(0, Math.floor(currArray.length/2)));
+		}
+		if(currArray.slice(Math.floor(currArray.length/2)+1).length > 0) {
+			q.enqueue(currArray.slice(Math.floor(currArray.length/2 + 1)));
+		}
+	}
+
+	return bst;
+}
+
+let arr1 = [1,2,3,4,5,6];
+let tree1 = minimal_tree(arr1);
+tree1.printInLevelOrder();
+
+let arr2 = [1,2,3,4,5,6,7];
+let tree2 = minimal_tree(arr2);
+tree2.printInLevelOrder();
+
+let arr3 = [2,3,6];
+let tree3 = minimal_tree(arr3);
+tree3.printInLevelOrder();
+
+let arr4 = [];
+let tree4 = minimal_tree(arr4);
+tree4.printInLevelOrder();
