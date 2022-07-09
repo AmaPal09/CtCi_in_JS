bfb41d4210393595e25cc69a796ba229f15d3f4b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-08 18:14:01 -0700

    Add problem 3 from chapter_4

---
+++ chapter_4/p4.3_list_of_depths.js
@@ -0,0 +1,24 @@
+/*
+List of depths
+Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+*/
+
+/*
+Explaination:
+*/
+
+/*
+Questions:
+1) How should the linked lists be returned. Would a list of lists work?
+*/
+
+/*
+Assumption:
+1)
+*/
+
+/*
+Example:
+1)
+*/
+
