950faaddb50e3fad32c65e3751f852130081a208
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-11 12:43:16 -0700

    Add LinkedList details

--- chapter_4/p4.3_list_of_depths.js
+++ chapter_4/p4.3_list_of_depths.js
@@ -57,3 +57,176 @@ class Queue {
 		}
 	}
 }
+
+
+class ListNode {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(head = null) {
+		this.head = head;
+	}
+
+	size_of_LL() {
+		let countLL = 0;
+		let currNode = this.head;
+		while(currNode) {
+			countLL ++ ;
+			currNode = currNode.next;
+		}
+
+		return countLL;
+	}
+
+	clearLL() {
+		this.head = null;
+	}
+
+	getLast() {
+		let currNode = this.head;
+		if (!currNode) {
+			return null;
+		}
+
+		while(currNode.next) {
+			currNode = currNode.next;
+		}
+		return currNode;
+
+	}
+
+	getFist() {
+		return this.head;
+	}
+
+	add(elem) {
+		if (this.head === null) {
+			this.head = new ListNode(elem);
+			return this.head.data;
+		}
+
+		let currNode = this.head;
+		while(currNode.next) {
+			currNode = currNode.next;
+		}
+
+		currNode.next = new ListNode(elem);
+		return currNode.next.data;
+	}
+
+	addMultiple(elemArray) {
+		for(let i = 0; i < elemArray.length; i++) {
+			this.add(elemArray[i]);
+		}
+	}
+
+	insertAt(elem, index) {
+		if (index < 0 || index > this.size_of_LL()) {
+			console.log("error in index");
+			return null;
+		}
+
+		let newNode = new ListNode(elem);
+		if (index == 0) {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		else {
+			let ctr = 0;
+			let currNode = this.head;
+			let prevNode;
+			while(ctr < index) {
+				prevNode = currNode;
+				currNode = currNode.next;
+				ctr ++;
+			}
+			prevNode.next = newNode;
+			newNode.next = currNode;
+		}
+	}
+
+	insertAfter(prevNode, elem) {
+		if(index < 0 || index > this.size_of_LL()){
+			console.log("Please enter a valid index");
+			return null;
+		}
+		let newNode = new ListNode(elem);
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+	}
+
+	removeFromIndex(index) {
+		if(index < 0 || inde x > this.size_of_LL()) {
+			console.log("Please enter valid index");
+		}
+
+		let currNode = this.head;
+		let prevNode = currNode;
+
+		if (index = 0) {
+			this.head = this.head.next;
+		}
+		else {
+			let ctr = 0;
+			while(ctr < index ) {
+				prevNode = currNode;
+				currNode = currNode.next;
+				ctr++;
+			}
+			prevNode.next = currNode.next;
+		}
+
+		return currNode.data;
+	}
+
+	removeElement(elem) {
+		let currNode = this.head;
+		let prevNode = null;
+
+		while(currNode != null) {
+			if(currNode.data === elem) {
+				if(prevNode === null) {
+					this.head = currNode.next;
+				}
+				else {
+					prevNode.next = currNode.next;
+				}
+
+				return currNode.data;
+			}
+			prevNode = currNode;
+			currNode = currNode.next;
+		}
+		return -1;
+	}
+
+	indexOf(elem) {
+		let ctr = 0;
+		let currNode = this.head;
+
+		while(currNode != null) {
+			if(currNode.data === elem) {
+				return ctr;
+			}
+			ctr++;
+			currNode = currNode.next;
+		}
+		return -1;
+	}
+
+	printList() {
+		let currNode = this.head;
+		let printArr;
+		while(currNode != null) {
+			printArr.push(currNode.data);
+			currNode = currNode.next;
+		}
+		return printArr.join("->");
+	}
+}
+
+
