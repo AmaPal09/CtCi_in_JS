0c5e1d4576bcd60a0e7bdc0a98b10c9beb416cda
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-11 15:07:12 -0700

    Add BST

--- chapter_4/p4.3_list_of_depths.js
+++ chapter_4/p4.3_list_of_depths.js
@@ -230,3 +230,53 @@ class LinkedList {
 }
 
 
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	};
+
+	insert(value) {
+		if(value < this.value) {
+			if(this.left === null) {
+				this.left = new BST(value);
+			}
+			else {
+				this.left.insert(value);
+			}
+		} else {
+			if(this.right === null) {
+				this.right = new BST(value);
+			} else {
+				this.right.insert(value);
+			}
+		}
+	};
+
+	printLevelOrder() {
+		let level = [];
+		let q = new Queue();
+		let nextQ = new Queue();
+		let currNode;
+
+		q.enqueue(this);
+		while(!q.isEmpty()) {
+			currNode = q.dequeue();
+			level.push()currNode.value;
+		}
+
+		if (currNode.left !== null) {
+			nextQ.enqueue(currNode.left);
+		}
+		if (currNode.right !== null) {
+			nextQ.enqueue(currNode.right);
+		}
+		if (q.isEmpty()) {
+			console.log(level.join(','));
+			level = [];
+			q = nextQ;
+			nextQ = new Queue();
+		}
+	}
+};
