882311bb1103493a7c590063f838e8ca917dc9a7
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-19 12:50:24 -0700

    Sol2 without extra DSs.

--- chapter_4/p4.8_first_common_ancestor.js
+++ chapter_4/p4.8_first_common_ancestor.js
@@ -26,7 +26,7 @@ Example:
 1)
 */
 
-//Not an actual solution
+//Not an actual solution uses extra DS
 // Find a path from root to n1 and store it in a DS
 // Find a path from root to n2 and store it in another DS
 // Traverse both DS until there is a mismatch. Return common element just before mismatch.
@@ -113,7 +113,60 @@ console.log("Common Ancestor for 4,5 = " + findLCA(4,5));
 console.log("Common Ancestor for 4,6 = " + findLCA(4,6));
 console.log("Common Ancestor for 3,4 = " + findLCA(3,4));
 console.log("Common Ancestor for 2,4 = " + findLCA(2,4));
+console.log("Common Ancestor for 2,8 = " + findLCA(2,8));
 
 //Time complexity = O(n). Tree traversed twice
 //Space 2 extra DSs used
+
+
+// Sol1
+// Explanation
+// Start traversal from root, if n1 or n2 matches the root then root is
+// the LCA. The assumption here is that both the nodes are present in the
+// tree.
+// If root does not match the either of the nodes then recure for left and
+// right subtrees.
+// Node that has one key present in its left subtree and other is present
+// in its right subtree is the LCA
+// If both nodes are present in the left subtrees, then it also has the LCA,
+// else LCA is in the right subtree.
+
+// *** ASSUMPTION ***
+// Both the nodes are present in the tree.
+// Values for n1 and n2 are provided not actual nodes
+
+// root = null;
+
+function findLCASol2(n1, n2) {
+	return findLCAWithRootSol2(root, n1, n2);
+}
+
+function findLCAWithRootSol2(node, n1, n2) {
+	if (node == null) {
+		return null;
+	}
+
+	if (node.value == n1 || node.value == n2) {
+		return node;
+	}
+
+	let lcaLeftSub = findLCAWithRootSol2(node.left, n1, n2);
+	let lcaRightSub = findLCAWithRootSol2(node.right, n1,n2);
+
+	if (lcaLeftSub != null && lcaRightSub != null) {
+		return node;
+	}
+
+	return lcaLeftSub != null ? lcaLeftSub : lcaRightSub;
+
+}
+
+console.log("Common Ancestor with Sol2 for 4,5 = " + findLCASol2(4,5).value);
+console.log("Common Ancestor with Sol2 for 4,6 = " + findLCASol2(4,6).value);
+console.log("Common Ancestor with Sol2 for 3,4 = " + findLCASol2(3,4).value);
+console.log("Common Ancestor with Sol2 for 2,4 = " + findLCASol2(2,4).value);
+console.log("Common Ancestor with Sol2 for 2,8 = " + findLCASol2(2,8).value);
+//Time Complexity O(n)
+// Issue: Assumption that both the nodes are present.
+
 
