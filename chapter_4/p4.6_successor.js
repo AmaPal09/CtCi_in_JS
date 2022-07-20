b9f8ee4f95c7a85e0d0d5e4bb798d0d1a6e4ba2b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-19 21:32:20 -0700

    Sol2 without using parent

--- chapter_4/p4.6_successor.js
+++ chapter_4/p4.6_successor.js
@@ -135,5 +135,73 @@ console.log("In-order successor of 10 is " + inOrderSuccessor(e1));
 console.log("In-order successor of 14 is " + inOrderSuccessor(f1));
 console.log("In-order successor of 22 is " + inOrderSuccessor(b1));
 
+//Time complexity O(n) n is the height of the tree.
+// Space 1 (no extra space used)
 
 
+
+// Method 2
+// Inorder successor without the parent.
+// Assumption
+// no parent property present on the tree node.
+// Both the root of the tree and the node are provided.
+// node is a part of the tree whose root is provided.
+
+//Sol
+//If right subtree is not null, then follow part of on method 1
+//If right subtree is null,
+//Start from the root of the tree, as seach through the tree.
+// Travel down the tree. Store the root as the succ.
+// if node's data is greater than the root, then
+// go right, make the right node the root.
+// else if node's data is smaller than the root, then  successor is the root,
+// and make the left node the root.
+class TreeNode {
+	constructor(data) {
+		this.value = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+const inOrderSuccessorSol2 = (root, node) => {
+
+	if (node.right != null) {
+		return getMin2(node.right);
+	}
+
+	let successor = null;
+	while (root != null) {
+		if (node.value < root.value) {
+			successor = root;
+			root = root.left;
+		}
+		else if (node.value > root.value) {
+			root = root.right;
+		}
+		else  {
+			break
+		};
+	}
+	return successor;
+}
+
+const getMin2 = (node) => {
+	while (node.left != null) {
+		node = node.left;
+	}
+	return node;
+}
+
+//20, 8, 22, 4, 12, 10, 14
+let r1 = new TreeNode(20);
+r1.left = new TreeNode(8);
+r1.right = new TreeNode(22);
+r1.left.left = new TreeNode(4);
+r1.left.right = new TreeNode(12);
+r1.left.right.left = new TreeNode(10);
+r1.left.right.right = new TreeNode(14);
+console.log("In-order successor sol2 of 8 is " + inOrderSuccessorSol2(r1, r1.left).value);
+console.log("In-order successor sol2 of 10 is " + inOrderSuccessorSol2(r1, r1.left.right.left).value);
+console.log("In-order successor sol2 of 14 is " + inOrderSuccessorSol2(r1, r1.left.right.right).value);
+console.log("In-order successor sol2 of 22 is " + inOrderSuccessorSol2(r1, r1.left.right.right).value);
