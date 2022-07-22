51ce26f18cf96aebbd7e3a91c637dcd9b8627fe6
Amarja Palekar <amarjapalekar09@gmail.com>
2022-07-21 17:39:52 -0700

    Sol for problem 9

--- chapter_4/p4.9_BST_Sequence.js
+++ chapter_4/p4.9_BST_Sequence.js
@@ -30,7 +30,172 @@ As sequence on the same element does not matter, that is
 		  2   3
 will result in {1,2,3} or {1,3,2}.
 So we need to weave all possible options for that level.
-
 */
 
 
+class TreeNode {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BST {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(value) {
+		let newNode = new TreeNode(value);
+		if (!this.root) {
+			this.root = newNode;
+		}
+		else {
+			this.addNode(this.root, newNode);
+		}
+	}
+	addNode(node, newNode) {
+		if (newNode.value < node.value) {
+			if (!node.left) {
+				node.left = newNode;
+			}
+			else {
+				this.addNode(node.left, newNode);
+			}
+		}
+		else if (newNode.value > node.value) {
+			if (!node.right) {
+				node.right = newNode;
+			}
+			else {
+				this.addNode(node.right, newNode);
+			}
+		}
+	}
+
+}
+
+let tree1 = new BST();
+tree1.insert(2);
+tree1.insert(1);
+tree1.insert(3);
+console.log(tree1);
+
+let tree2 = new BST();
+tree2.insert(50);
+tree2.insert(20);
+tree2.insert(60);
+tree2.insert(10);
+tree2.insert(25);
+tree2.insert(70);
+tree2.insert(5);
+tree2.insert(15);
+tree2.insert(65);
+tree2.insert(80);
+// console.log(tree2);
+
+
+function allSequences(node) {
+	let result = [];
+
+	if (!node) {
+		result.push([]);
+		return result;
+	}
+
+	let prefix = [];
+	prefix.push(node.value);
+	// console.log(1, prefix);
+	//Recurse through left and right subtrees
+	let leftSeq = allSequences(node.left);
+	let rightSeq = allSequences(node.right);
+	// console.log(2, leftSeq, rightSeq);
+
+	//Weave together each list from the left and the right
+	for (let left of leftSeq) {
+		for (let right of rightSeq) {
+			const weaved = [];
+			// console.log(left, right, weaved, prefix);
+			weaveLists(left, right, weaved, prefix);
+			// console.log(left, right, weaved, prefix);
+			result.push(...weaved);
+			// console.log(result);
+		}
+	}
+
+	return result;
+}
+
+function weaveLists(first, second, results, prefix) {
+	//If either of the lists is empty, add remainder of the other list
+	//to a cloned prefix and store the results.
+	if(first.length == 0 || second.length == 0) {
+		// console.log(4, first, second, results,prefix);
+		const tempResult = prefix.slice(0);
+		tempResult.push(...first);
+		tempResult.push(...second);
+		results.push(tempResult);
+		return ;
+	}
+
+	//Recurr with head of the first added to the prefix. Removing the head
+	// will damange first, so we'll need to put it back where we found it
+	// afterwards.
+	let tempHeadFirst = first.shift();
+	prefix.push(tempHeadFirst);
+	weaveLists(first, second, results, prefix);
+	prefix.pop();
+	first.unshift(tempHeadFirst);
+
+	//Recurr with head of the second added to the prefix. Removing the head
+	// will damange second, so we'll need to put it back where we found it
+	// afterwards.
+	let tempHeadSecond = second.shift();
+	prefix.push(tempHeadSecond);
+	weaveLists(first, second, results, prefix);
+	prefix.pop();
+	second.unshift(tempHeadSecond);
+}
+
+
+// const allSequences = node => {
+//   const result = [];
+
+//   if (!node) {
+//     result.push([])
+//     return result;
+//   }
+//   const prefix = [];
+//   prefix.push(node.value);
+//   console.log(1, prefix);
+//   /* Recurse on left and right subtrees. */
+//   const leftSeq = allSequences(node.left);
+//   const rightSeq = allSequences(node.right);
+//   console.log(2, leftSeq, rightSeq);
+//   Weave together each list from the left and right sides.
+//   for (let left of leftSeq) {
+//     for (let right of rightSeq) {
+//       const weaved = [];
+//       console.log(left, right, weaved, prefix);
+//       weaveLists(left, right, weaved, prefix);
+//       console.log(left, right, weaved, prefix);
+//       result.push(...weaved);
+//       console.log(result);
+//     }
+//   }
+//   return result
+// }
+
+
+
+console.log(allSequences(tree1.root));
+// console.log(allSequences(tree2.root));
+let tree3 = new BST();
+tree3.insert(20);
+tree3.insert(10);
+tree3.insert(30);
+tree3.insert(5);
+tree3.insert(15);
+tree3.insert(25);
+console.log(allSequences(tree3.root));
